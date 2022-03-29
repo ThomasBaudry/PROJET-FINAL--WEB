ee159a9f87f56f84898be3e4d61d451c8615355d
Thomas Baudry <2132465@cegeprdl.ca>
2022-03-29 10:33:55 -0400

    Ecriture code liaison avec l'API.

--- PROJET FINAL -WEB/Controllers/GarderieController.cs
+++ PROJET FINAL -WEB/Controllers/GarderieController.cs
@@ -3,6 +3,9 @@ using System.Json;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using GestionCegepWeb.Utils;
+using PROJET_FINAL___API.Logics.DTOs;
+using System;
 
 namespace PROJET_FINAL__WEB.Controllers
 {
@@ -14,40 +17,101 @@ namespace PROJET_FINAL__WEB.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            try
+            {
+                //Préparation des données pour la vue...
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie");
+                ViewBag.ListeGarderie = JsonConvert.DeserializeObject<List<GarderieDTO>>(jsonResponse.ToString()).ToArray();
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
             return View();
         }
 
         [Route("/Garderie/AjouterCegep")]
         [HttpPost]
-        public async Task<IActionResult> AjouterGarderie()
+        public async Task<IActionResult> AjouterGarderie([FromForm] GarderieDTO garderieDTO)
         {
-            return View();
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/AjouterGarderie", garderieDTO);
+            }
+            catch (Exception e)
+            {
+                TempData["MessageErreur"] = e.Message;
+            }
+
+            //Lancement de l'action Index...
+            return RedirectToAction("Index", "Garderie");
         }
 
         [Route("/Garderie/FormulaireModifierGarderie")]
         [HttpGet]
-        public async Task<IActionResult> FormulaireModifierGarderie()
+        public async Task<IActionResult> FormulaireModifierGarderie([FromQuery] string nomGarderie)
         {
-            return View();
+            try
+            {
+                if (TempData["MessageErreur"] != null)
+                    ViewBag.MessageErreur = TempData["MessageErreur"];
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/ObtenirGarderie?nomGarderie=" + nomGarderie);
+                GarderieDTO cegep = JsonConvert.DeserializeObject<GarderieDTO>(jsonResponse.ToString());
+                return View(cegep);
+            }
+            catch (Exception e)
+            {
+                TempData["MessageErreur"] = "Impossible d'ouvrir le formulaire de modification. " + e.Message;
+            }
+            return RedirectToAction("Index");
         }
 
         [Route("/Garderie/ModifierGarderie")]
         [HttpPost]
-        public async Task<IActionResult> ModifierGarderie()
+        public async Task<IActionResult> ModifierGarderie([FromForm] GarderieDTO garderieDTO)
         {
-            return View();
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/ModifierGarderie", garderieDTO);
+            }
+            catch (Exception e)
+            {
+                TempData["MessageErreur"] = "Impossible de procéder à la modification." + e.Message;
+                return RedirectToAction("FormulaireModifierGarderie", "Cegep", new { nomGarderie = garderieDTO.Nom });
+            }
+            //Lancement de l'action Index...
+            return RedirectToAction("Index");
         }
         [Route("/Garderie/SupprimerGarderie")]
         [HttpPost]
-        public async Task<IActionResult> SupprimerGarderie()
+        public async Task<IActionResult> SupprimerGarderie([FromForm] string nomGarderie)
         {
-            return View();
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/SupprimerGarderie?nomGarderie=" + nomGarderie, null);
+            }
+            catch (Exception e)
+            {
+                TempData["MessageErreur"] = e.Message;
+            }
+
+            return RedirectToAction("Index");
         }
 
         [Route("/Garderie/ViderListeGarderie")]
         [HttpPost]
         public async Task<IActionResult> ViderListeGarderie()
         {
-            return View();
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/ViderListeGarderie", null);
+            }
+            catch (Exception e)
+            {
+                TempData["MessageErreur"] = e.Message;
+            }
+
+            return RedirectToAction("Index");
         }
     }
+}
