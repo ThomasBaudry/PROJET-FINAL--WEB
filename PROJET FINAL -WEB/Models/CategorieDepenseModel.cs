3fcaf0f92c5f3ae71d58f09d893ca958b0ca161a
quentin guillou <quentin.guillou49@gmail.com>
2022-04-05 09:57:15 -0400

    Création du controller et des models

---
+++ PROJET FINAL -WEB/Models/CategorieDepenseModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJET_FINAL__WEB.Models
+{
+    public class CategorieDepenseDTO
+    {
+        #region Proprietes
+        /// <summary>
+        /// Propriété représentant la description de la Catégorie de Dépense.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Propriété représentant le pourcentage de la Catégorie de Dépense.
+        /// </summary>
+        public string Pourcentage { get; set; }
+
+        #endregion Proprietes
+
+        #region Constructeurs
+
+        public CategorieDepenseDTO() { }
+
+        /// <summary>
+        /// Constructeur avec paramètres.
+        /// </summary>
+        /// <param name="description">Nom de la Catégorie de Dépense.</param>
+        /// <param name="pourcentage">Pourcentage de la Catégorie de Dépense.</param>
+        public CategorieDepenseDTO(string description = "", string pourcentage = "")
+        {
+            Description = description;
+            Pourcentage = pourcentage;
+        }
+
+        #endregion Constructeurs
+    }
+}
---
+++ PROJET FINAL -WEB/Models/DepenseDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJET_FINAL__WEB.Models
+{
+    public class DepenseDTO
+    {
+        #region Proprietes
+        /// <summary>
+        /// Propriété représentant la date du moment de la Dépense.
+        /// </summary>
+        public string DateTemps { get; set; }
+        /// <summary>
+        /// Propriété représentant le montant de la Dépense.
+        /// </summary>
+        public string Montant { get; set; }
+        /// <summary>
+        /// Propriété représentant le montant admissible de la Dépense (= montant * categorie depense).
+        /// </summary>
+        public string MontantAdmissible { get; set; }
+        /// <summary>
+        /// Propriété reliant avec le Commerce.
+        /// </summary>
+        public CommerceDTO commerceDTO { get; set; }
+        /// <summary>
+        /// Propriété reliant avec la Catégorie de Dépenses).
+        /// </summary>
+        public CategorieDepenseDTO categorieDepenseDTO { get; set; }
+
+        #endregion Proprietes
+
+        #region Constructeurs
+
+        public DepenseDTO() { }
+
+        /// <summary>
+        /// Constructeur avec paramètres.
+        /// </summary>
+        /// <param name="dateTemps">Date du moment de la Dépense.</param>
+        /// <param name="montant">Montant de la Dépense.</param>
+        /// <param name="montantAdmissible">Montant admissible de la Dépense.</param>
+        public DepenseDTO(string dateTemps = "", string montant = "", string montantAdmissible = "")
+        {
+            DateTemps = dateTemps;
+            Montant = montant;
+            MontantAdmissible = montantAdmissible;
+        }
+
+        #endregion Constructeurs
+    }
+}
